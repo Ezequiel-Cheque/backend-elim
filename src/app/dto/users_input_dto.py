ff12ed8a314466981cb04a63c84dee448d114976
Ezequiel-Cheque <delaluzezequiel@gmail.com>
2023-11-17 13:24:36 -0600

    remove emailstr

--- src/app/dto/users_input_dto.py
+++ src/app/dto/users_input_dto.py
@@ -1,12 +1,12 @@
 import json
 
 from fastapi.encoders import jsonable_encoder
-from pydantic import BaseModel, Field, EmailStr
+from pydantic import BaseModel, Field
 
 class user_create_schema(BaseModel):
     name: str = Field(description="")
     last_name: str = Field(description="")
-    email: EmailStr = Field(description="")
+    email: str = Field(description="")
     phone: str = Field(description="", min_length=10)
     age: int = Field(description="")
     church: str = Field(description="")
